a6d9a7b3be30116e7049f5e992bd1eeaf10c72a1
Robert Santana <123roabel@gmail.com>
2023-10-19 10:36:05 -0400

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '15'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+		return (0);
+}
