5f3a4553b70c7c5501fea6fdec79bf1b986a5e62
Robert Santana <123roabel@gmail.com>
2023-10-19 12:59:36 -0400

    Print comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+
+		if (i < 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+}
