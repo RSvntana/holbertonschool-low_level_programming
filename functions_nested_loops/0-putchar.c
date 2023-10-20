5cdbe83c06c30783e87afe0a8a2562810f7fffbf
Robert Santana <123roabel@gmail.com>
2023-10-20 08:59:03 -0400

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (0);
+}
+
