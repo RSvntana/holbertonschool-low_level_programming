90d3aaf02b152a6ef9bae045ddd7e41a550206da
Robert Santana <123roabel@gmail.com>
2023-10-20 09:31:40 -0400

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  *  main - prints the string "Holberton"
+ *
  *  Return: always 0 (success)
  */
-
 int main(void)
 {
-
 	char *c;
 
 	int i;
+
 	c = "_putchar";
 
 	for (i = o; i < 8; i++)
