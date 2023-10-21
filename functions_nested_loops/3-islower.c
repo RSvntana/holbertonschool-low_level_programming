e64272e26db0953656ca9f80cfd1ad7e2c5b6e6e
Robert Santana <123roabel@gmail.com>
2023-10-21 16:26:01 -0400

    Islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - return 1 if c is lowecase and return 0 otherwise
+ * @c: is the imput character
+ * Return: 2.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+
+	else
+
+		return (0);
+}
