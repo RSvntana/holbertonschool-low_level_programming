1a4a6a2b5cb09dc1701c9e45e37bd47f112be6bb
Robert Santana <123roabel@gmail.com>
2023-10-22 15:07:18 -0400

    Print lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#inlcude "main.h"
+/**
+ * print_line - Draws a stright line according to parameter
+ * @n: the number or lines to draw
+ * Return: always
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		fot (c = 0; c < n; c++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
