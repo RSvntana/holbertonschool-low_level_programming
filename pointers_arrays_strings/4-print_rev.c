a6967adfacd1d0b296d2cefc386b97bbf6979dc8
Robert Santana <123roabel@gmail.com>
2023-10-25 14:14:05 -0400

    Print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int length = strlen(s);
+	int i;
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
-		printf("%c", s[i]);
+		_putchar(*(s + i));
 	}
-	printf("\n");
+	_putchar("\n");
 }
