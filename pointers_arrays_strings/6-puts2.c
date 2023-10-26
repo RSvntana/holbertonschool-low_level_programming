2710b6ee16c040c1370c9493e8600c08fd6f403d
Robert Santana <123roabel@gmail.com>
2023-10-25 21:34:00 -0400

    Puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _puts2 - print every other character of a string
+ * @str: string to put twice
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
