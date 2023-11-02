e52c00e53b04f41d0bdcd96a0676dab65158d166
Robert Santana <123roabel@gmail.com>
2023-11-02 13:31:40 -0400

    Strlen Recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns length
+ * @s: character pointer
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	int (*s == '\0')
+	{
+	return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+
+}
