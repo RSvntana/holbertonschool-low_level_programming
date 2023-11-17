a3f2b2e448f14376c800c7a66b72f7621b17821c
Robert Santana <123roabel@gmail.com>
2023-11-17 12:21:59 -0400

    New Dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,85 +1,48 @@
+#include <stdio.h>
 #include "dog.h"
+#include <string.h>
 #include <stdlib.h>
-
 /**
- *_strlen - prints the length of a string
- *@s: string to be counted
- *Return: length of string
+ *new_dog - creates a new dog
+ *@name: the name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner
+ *Return: Return a pointer to the new dog
  */
-int _strlen(char *s)
 
-
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * _strncpy - a function ...
- * @dest: the chaine
- * @src: the chaine
- * @n: the number
- *
- * Return: 1 or 0
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n && src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
-}
-
-/**
- * new_dog - create new dog
- * @name: new dog name
- * @age: new dog age
- * @owner: new dog owner
- *
- * Return: new dog
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ndog;
+	dog_t *newDog = malloc(sizeof(dog_t));
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
-		return (NULL);
-
-	ndog->name = malloc(_strlen(name) + 1);
-	if (ndog->name == NULL)
+	if (newDog == NULL)
 	{
-		free(ndog);
 		return (NULL);
 	}
-
-	ndog->owner = malloc(_strlen(name) + 1);
-	if (ndog->owner == NULL)
+	else
 	{
-		free(ndog->name);
-		free(ndog);
-		return (NULL);
+		newDog->name = malloc(strlen(name) + 1);
+		if (newDog->name == NULL)
+		{
+			free(newDog);
+			return (NULL);
+		}
+		else
+		{
+			strcpy(newDog->name, name);
+		}
+		newDog->age = age;
+
+		newDog->owner = malloc(strlen(owner) + 1);
+		if (newDog->owner == NULL)
+		{
+			free(newDog->name);
+			free(newDog);
+			return (NULL);
+		}
+		else
+		{
+			strcpy(newDog->owner, owner);
+		}
 	}
-
-	ndog->name = name;
-	ndog->age = age;
-	ndog->owner = owner;
-
-	return (ndog);
+	return (newDog);
 }
