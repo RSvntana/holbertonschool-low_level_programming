4ef77d0d5dd7981e777efee0d83c3a8dc2cb9921
Robert Santana <123roabel@gmail.com>
2023-12-08 10:20:08 -0400

    read textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,54 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+/**
+ * read_textfile - reads a textfile
+ * @filename: file to read
+ * @letters: amount of bytes to read
+ * Return: printed letters count
+ */
 
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int rd, wr, fd;
+	char *buff = malloc(sizeof(char *) * letters);
 
-    char *buffer = (char *)malloc(letters + 1);
-    if (buffer == NULL) {
-        fclose(file);
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+		return (0);
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr == -1)
+		return (0);
 
-    ssize_t bytesRead = fread(buffer, 1, letters, file);
+	free(buff);
+	close(fd);
 
-    if (bytesRead <= 0) {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
-
-    buffer[bytesRead] = '\0';
-
-    ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-
-    free(buffer);
-    fclose(file);
-
-    if (bytesWritten != bytesRead) {
-        return 0;
-    }
-
-    return bytesWritten;
-}
-
-int main() {
-    const char *filename = "example.txt";
-
-    ssize_t result = read_textfile(filename, 100);
-
-    if (result == 0) {
-        fprintf(stderr, "Error reading or printing the file.\n");
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+	return (wr);
 }
