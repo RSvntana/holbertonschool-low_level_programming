ea5fff537279a54a456163a60e6df997bd3489d5
Robert Santana <123roabel@gmail.com>
2023-12-08 10:54:07 -0400

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name for the file
+ * @text_content: the content of the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (!text_content)
+		return(1);
+	if (write(fd, text_content, strlen(text_content)) == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
