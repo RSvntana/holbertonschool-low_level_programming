a1605c7d306c595e42df895a9d1861eab2315707
Robert Santana <123roabel@gmail.com>
2023-12-09 16:34:20 -0400

    hash table create

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -7,6 +7,19 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
