c75bd38a405072ee7501514dbda2377b3a4ac84e
Robert Santana <123roabel@gmail.com>
2024-01-18 19:32:02 -0400

    create task 2

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,42 +1,52 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array using binary search.
+ * binary_search_recursive - Searches for a value in a sorted array using binary search.
  * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
+ * @start: Start index of the subarray.
+ * @end: End index of the subarray.
  * @value: Value to search for.
  *
  * Return: The index where the value is located,
  *         or -1 if the value is not present in the array or if the array is NULL.
  */
-
-int binary_search(int *array, size_t size, int value)
+int binary_search_recursive(int *array, size_t start, size_t end, int value)
 {
-    size_t left = 0, right = size - 1, mid;
+    size_t mid, index;
 
-    if (array == NULL)
-        return (-1);
-
-    while (left <= right)
+    while (start <= end)
     {
-        mid = (left + right) / 2;
+        printf("Searching in array:");
+        for (index = start; index < end; index++)
+            printf(" %d,", array[index]);
+        printf(" %d\n", array[index]);
 
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-        {
-            if (i == right)
-                printf("%d\n", array[i]);
-            else
-                printf("%d, ", array[i]);
-        }
+        mid = (start + end) / 2;
 
         if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
+            return mid; // Value found, return the index
+        if (array[mid] < value)
+            start = mid + 1; // Search in the right half
         else
-            right = mid - 1;
+            end = mid - 1; // Search in the left half
     }
 
-    return (-1);
+    return -1; // Value not found
+}
+
+/**
+ * binary_search - Wrapper function for binary_search_recursive.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
+ *
+ * Return: The index where the value is located,
+ * or -1 if the value is not present in the array or if the array is NULL.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+    return binary_search_recursive(array, 0, size - 1, value);
 }
