6e23e6cc5fcd7c5773985e9c0e3999bdcadf2d9d
Robert Santana <123roabel@gmail.com>
2024-01-18 04:26:28 -0400

    fix task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,11 +11,13 @@
  */
 
 int linear_search(int *array, size_t size, int value) {
+	size_t i;
+
 	if (array == NULL) {
 		return (-1);
 	}
 
-	for (size_t i = 0; i < size; i++) {
+	for (i = 0; i < size; i++) {
 		printf("Value compared: %d\n", array[i]);
 
 		if (array[i] == value) {
@@ -25,3 +27,4 @@ int linear_search(int *array, size_t size, int value) {
 
 	return (-1);
 }
+
