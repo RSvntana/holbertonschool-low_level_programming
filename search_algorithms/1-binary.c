5c121793333182384991a27c404109dfbabf58c8
Robert Santana <123roabel@gmail.com>
2024-01-18 19:34:49 -0400

    create task 2

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,6 +10,7 @@
  * Return: The index where the value is located,
  *         or -1 if the value is not present in the array or if the array is NULL.
  */
+
 int binary_search_recursive(int *array, size_t start, size_t end, int value)
 {
     size_t mid, index;
@@ -24,14 +25,14 @@ int binary_search_recursive(int *array, size_t start, size_t end, int value)
         mid = (start + end) / 2;
 
         if (array[mid] == value)
-            return mid; // Value found, return the index
+            return (mid);
         if (array[mid] < value)
-            start = mid + 1; // Search in the right half
+            start = mid + 1;
         else
-            end = mid - 1; // Search in the left half
+            end = mid - 1;
     }
 
-    return -1; // Value not found
+    return (-1);
 }
 
 /**
@@ -43,10 +44,11 @@ int binary_search_recursive(int *array, size_t start, size_t end, int value)
  * Return: The index where the value is located,
  * or -1 if the value is not present in the array or if the array is NULL.
  */
+
 int binary_search(int *array, size_t size, int value)
 {
     if (array == NULL)
-        return -1;
+        return (-1);
 
-    return binary_search_recursive(array, 0, size - 1, value);
+    return (binary_search_recursive(array, 0, size - 1, value));
 }
